b2be841fc7a7a0c990c660d86893854bdd007ab3
lvaka <30805557+lvaka@users.noreply.github.com>
2017-08-23 18:26:56 -0700

    8.23
    Cleaned up code for easier readability.

--- Place_Orders.py
+++ Place_Orders.py
@@ -1,62 +1,80 @@
 #Importing CSV library to read file
 import csv
 
-runStatus = 1 #Keep Looping if 1. Terminate if 0
+#Returns the Correct Order List
+def listSelector (choice):
+	if choice == 1:
+		return 'meats.csv'
+	elif choice == 2:
+		return 'ifs.csv'
+	elif choice == 3:
+		return 'sysco.csv'
 
-while runStatus == 1:
-	orderList = [] # This will store the arrays for Ordering
-	option = 0 # This is the selector for Order Listile
-	csvFile = '' #This will be the filename of the selection
-	orderAgain = '' #Condition to loop
-	
-	#Presenting User with Choice
-	while option == 0:
+		#Picks the correct CSV file to open
+def selectionMenu ():
+	selection = 0
+	while selection == 0:
 		print ("Welcome to the Order Script, what would you like to place orders for?")
 		print ("1)Rocker Bros  2)IFS  3)Sysco")
-		option = int(input('Your Choice? '))
+		selection = int(input('Your Choice? '))
 		
-		if (option < 0 or option > 4):
-			option = 0
+		if (selection < 0 or selection > 4):
+			selection = 0
 			print ("I'm sorry, that's an invalid entry.  Please input again")
 		else:
 			break
+	return selection
 
-	#Cases for csvFile
-	if option == 1:
-		csvFile = 'meats.csv'
-	elif option == 2:
-		csvFile = 'ifs.csv'
-	elif option == 3:
-		csvFile = 'sysco.csv'
-
-
-	#Reading CSV File and loading into Data
-	with open(csvFile) as csvfile:
+#Requests User for quantity they would like to order
+def placeOrder(fileName):
+	userInputList = []
+	with open(fileName) as csvfile:
 		readCSV = csv.reader(csvfile, delimiter=',')
 		for row in readCSV:
 			print(row[0], row[1])
 			quantity = input("How much do you want to order? ")
+			#loops and asks again if quantity is empty
+			while quantity == "":
+				quantity = input("How much do you want to order? ")
 			row.append(quantity) #Adds User Requested Quantity into array
-			orderList.append(row)
-			
-	print ("Hello, I will be placing an order for Chego Tomorrow. For tomorrow we will need...")		
-	for section in orderList:
-		if(int(section[2]) > 0):
-			print(section[2], 'x', section[1], section [0])
-
-	print ("Thanks.  Have a blessed day")
-
-	print ("")
+			userInputList.append(row)
 	
+	return userInputList
+
+#Assembles final string for order
+def assembleOrder(orderingList):
+	orderString = "Hello, I will be placing an order for Chego Tomorrow. For tomorrow we will need... \n"		
+	for section in orderingList:
+		if(int(section[2]) > 0):
+			orderString += section[2] + ' x ' + section[1] + ' ' + section[0] + '\n'
+
+	orderString += "Thanks.  Have a blessed day \n"
+	return orderString
+
+#Asks user if they want to palce another order
+def askAnotherOrder():
+	orderAgain = ''
 	while orderAgain == '':
 		orderAgain = input ("Would You like to place another order? YES/NO?")
 		if orderAgain.lower() == 'yes':
-			runStatus = 1
+			return 1
 		elif orderAgain.lower() == 'no':
-			runStatus = 0
+			return 0
 			break
 		else:
 			print("I'm sorry.  Invalid Input.")
 			orderAgain = ''
 
+#Runs the ordering process while runStatus is 1
+def orderEngine(runStatus):			
+	while runStatus == 1:
+		option = selectionMenu()
+		csvFile = listSelector(option)
+		orderList = placeOrder(csvFile)
+		finalOrder = assembleOrder(orderList)
 		
+		print (finalOrder)
+
+		runStatus = askAnotherOrder()
+		
+orderEngine(1)
